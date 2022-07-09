869e271f64e12b264440c64d72ad2c3f559bc66d
Aimi <aimitranhoang@gmail.com>
2022-07-08 21:00:41 -0400

    Created the landing page.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import { CacheProvider } from '@emotion/react';
-import theme from '../themes/theme';
-import createEmotionCache from '../themes/createEmotionCache';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import theme from "../themes/theme";
+import createEmotionCache from "../themes/createEmotionCache";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -13,20 +13,60 @@ export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    //This is the original version of Webapp. We not remove it till the website working
+    // <CacheProvider value={emotionCache}>
+    //   <Head>
+    //     <meta name="viewport" content="initial-scale=1, width=device-width" />
+    //   </Head>
+    //   <ThemeProvider theme={theme}>
+    //     <CssBaseline />
+    //     <Component {...pageProps} />
+    //   </ThemeProvider>
+    // </CacheProvider>
+    <div className=" min-h-screen">
+      <nav className="flex justify-center bg-white border-2 py-3 ">
+        Nav bar goes here
+      </nav>
+
+      {/* Main content */}
+      <div className="container h-[80vh] mx-auto relative flex flex-col items-center justify-center ">
+        <div className="flex gap-5 justify-center items-center mb-6 mx-8">
+          <img
+            src={dscLogo}
+            alt="dscLogo"
+            className="h-7 w-auto md:h-10 lg:h-12"
+          />
+          <h1 className="text-gray-600 text-3xl md:text-4xl lg:text-6xl font-normal ">
+            Google Developer Student Club
+          </h1>
+        </div>
+        <div className="text-gray-500 text-2xl md:text-3xl lg:text-4xl font-normal mb-10">
+          Kennesaw State University
+        </div>
+        <div className="flex justify-center items-center gap-5  text-pink-400 mb-10 text-sm  lg:text-2xl">
+          <h3>#developerstudentclubs</h3>
+          <h3>#googlestudents</h3>
+          <h3>#growwithgoogle</h3>
+        </div>
+        {/* buttons*/}
+        <div className=" buttons flex justify-center content-center items-center ">
+          <Button href="#" variant="contained">
+            Get Started
+          </Button>
+        </div>
+      </div>
+      {/* Wave */}
+      <div className="bg-[#42a5f5ff] h-10 opacity-100 absolute bottom-0 ">
+        <div className="bg-[url('./svg/wave.svg')] bg-repeat-x absolute bottom-0 w-[6400px] h-[198px] animate-wave1 translate-x-[0] translate-y-[0] translate-z-[0] z-20"></div>
+        <div className="bg-[url('./svg/wave.svg')] bg-repeat-x absolute bottom-[0px] w-[6400px] h-[198px] animate-wave2 translate-x-[0] translate-y-[0] translate-z-[0] opacity-90 z-10"></div>
+        <div className="bg-[url('./svg/wave.svg')] bg-repeat-x absolute bottom-[0px] w-[6400px] h-[198px] animate-wave2 translate-x-[0] translate-y-[0] translate-z-[0] opacity-60 z-10"></div>
+      </div>
+    </div>
   );
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   emotionCache: PropTypes.object,
-  pageProps: PropTypes.object.isRequired
+  pageProps: PropTypes.object.isRequired,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,35 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./fonts/GoogleSans-Bold-v1.27.ttf";
+import "./fonts/GoogleSansDisplay-Regular-v1.27.ttf";
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> with{" "}
-          <a href="https://mui.com/">Material-UI!</a>
-        </h1>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-      </main>
-    </div>
-  )
-}
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main className={styles.main}>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Next.js!</a> with{" "}
+//           <a href="https://mui.com/">Material-UI!</a>
+//         </h1>
+
+//         <p className={styles.description}>
+//           Get started by editing{' '}
+//           <code className={styles.code}>pages/index.js</code>
+//         </p>
+//       </main>
+//     </div>
+//   )
+// }
