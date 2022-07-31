8554d8550e4a4713902605dffc4d273d536050b3
theTechOwl <david.e.chavarro@outlook.com>
2022-07-31 17:46:19 -0400

    Implement Navbar (PC compatible)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,76 +1,38 @@
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Stack,
-  Button,
-  Box,
-  useMediaQuery,
-  useTheme,
-  Tabs,
-  Tab,
-} from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material"
+import { useSession, signIn, signOut } from 'next-auth/react';
 
-import { useSession, signIn, signOut } from "next-auth/react";
-import React, { useState, useEffect } from "react";
-import DrawerComp from "./DrawerComp";
-
-const PAGES = ["About", "Events", "Socials", "Our Team"];
 const Navbar = (props) => {
-  const { data: session, status } = useSession();
-  const [value, setValue] = useState();
-  const theme = useTheme();
-  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+    const { data: session, status } = useSession();
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          {isMatch ? (
-            <>
-              <DrawerComp sx={{ marginLeft: 4 }} />
-              <Box
-                component="img"
-                sx={{ height: 50, width: 50, mx: "auto" }}
-                alt="Google Developer Student Club Logo"
-                src="/static/images/gdsc_logo.png"
-              />
-            </>
-          ) : (
-            <>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                DSC KSU
-              </Typography>
-              <Tabs textColor="#000">
-                {PAGES.map((page, index) => (
-                  <Tab key={index} label={page}></Tab>
-                ))}
-              </Tabs>
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1}}>
+                    DSC KSU
+                </Typography>
+                <Stack direction='row' spacing={2}>
+                    <Button color='inherit' href="../about">About</Button>
+                    <Button color='inherit' href="../events">Events</Button>
+                    <Button color='inherit' href="../socials">Socials</Button>
+                    <Button color='inherit' href="../our_team">Our Team</Button>
 
-              {/* If Session does not exist, invalid or client has not logged in, do this. */}
-              {!session && (
-                <>
-                  <Button color="inherit" onClick={() => signIn()}>
-                    Login
-                  </Button>
-                </>
-              )}
+                     {/* If Session does not exist, invalid or client has not logged in, do this. */}
+                    {!session && (
+                        <>
+                            <Button color='inherit' onClick={() => signIn()}>Login</Button>
+                        </>
+                    )}
 
-              {/* If Session does exist and the client has logged in, do this. */}
-              {session && (
-                <>
-                  <Button color="inherit" onClick={() => signOut()}>
-                    Logout
-                  </Button>
-                </>
-              )}
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </>
-  );
-};
+                    {/* If Session does exist and the client has logged in, do this. */}
+                    {session && (
+                        <>
+                            <Button color='inherit' onClick={() => signOut()}>Logout</Button>
+                        </>
+                    )}
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
+}
 
 export default Navbar
