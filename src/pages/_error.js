42dd1baa4f36a4d67c23525f4d5a8ae851bbdee4
Sanjay-B <sanjaybhadra1821@gmail.com>
2022-08-18 02:41:39 -0400

    Improve error page design, Better descriptive errors

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,22 +1,36 @@
-import Head from 'next/head';
+import { Container, Stack, Typography, Button, Link } from '@mui/material';
+import { useTheme } from "@mui/material/styles"
 
-export default function Error() {
+// statusCode returns 'undefined' in development environment
+export default function Error({ statusCode }) {
+    const theme = useTheme()
+
+    // Custom Error Message based on Status Code
+    const errorStatus = {
+        '400': 'Bad Request',
+        '401': 'Unauthorized',
+        '403': 'Forbidden',
+        '404': 'Page Not Found',
+        '405': 'Method Not Allowed',
+        '408': 'Request Timeout',
+        '429': 'Too Many Requests',
+        '500': 'Internal Server Error',
+        '501': 'Not Implemented',
+        '502': 'Bad Gateway'
+    }
+
     return (
-        <div>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <h1>
-                     This page indicates an{' '}
-                     <code>Error</code>
-                </h1>
-                <p>
-                    Description:{' '}
-                    <code>500 - Internal Server Error</code>
-                </p>
-            </main>
-        </div>
+        <Container maxWidth='lg'>
+            <Stack mt={25} spacing={5} justifyContent='center' alignItems='center' textAlign='center'>
+                <Typography variant='h3'>{errorStatus[statusCode] !== undefined ? errorStatus[statusCode] : '500 Internal Server Error'}</Typography>
+                <Typography variant='body' maxWidth='560px'>
+                    Oops, we can't seem to complete the action you requested.
+                    Try going back to the previous page or click the button below
+                    to go back to the{' '}
+                    <Link href='/' underline='none' color={ theme.palette.secondary.main } sx={{ fontWeight: 'medium' }}>Landing Page</Link>
+                </Typography>
+                <Button variant='contained' color='secondary' rel="noreferrer" href='https://discord.gg/jQpFr6ZRXg'>Get Help</Button>
+            </Stack>
+        </Container>
     )
 }
