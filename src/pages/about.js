6193ec5ba0569d5db7a1eda309956651cfac1c06
Aimi <90480687+aimiisme@users.noreply.github.com>
2022-08-23 20:11:25 -0400

    Implement Responsive Navbar (mobile) and Auto-rescaling Web Content (#24)
    * Bump minimist from 1.2.5 to 1.2.6 in /src
    
    Bumps [minimist](https://github.com/substack/minimist) from 1.2.5 to 1.2.6.
    - [Release notes](https://github.com/substack/minimist/releases)
    - [Commits](https://github.com/substack/minimist/compare/1.2.5...1.2.6)
    
    ---
    updated-dependencies:
    - dependency-name: minimist
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump next-auth from 4.2.1 to 4.9.0 in /src
    
    Bumps [next-auth](https://github.com/nextauthjs/next-auth) from 4.2.1 to 4.9.0.
    - [Release notes](https://github.com/nextauthjs/next-auth/releases)
    - [Changelog](https://github.com/nextauthjs/next-auth/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/nextauthjs/next-auth/compare/v4.2.1...next-auth@v4.9.0)
    
    ---
    updated-dependencies:
    - dependency-name: next-auth
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * adding responsive navbar feature and rescaling the main content
    
    * Replacing img tag with html tags and re-formating
    
    * adding about pages
    
    * Implement Button, Page mappings for Navbar.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Sanjay <Sanjay-B@users.noreply.github.com>
    Co-authored-by: Sanjay-B <sanjaybhadra1821@gmail.com>

---
+++ src/pages/about.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Stack,
+  Grid,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
+
+const About = () => (
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="100vh"
+    sx={{ height: "75%" }}
+  >
+    <Card
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Stack
+        direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
+        spacing={{ xs: 1, md: 3 }}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CardContent sx={{ m: "30" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            About Us
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Google Developer Student Clubs (GDSC) are community groups for
+            university students interested in Google developer technologies.
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Students from all undergraduate or graduate programs with an
+            interest in growing as a developer are welcome.
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            By joining a GDSC, students grow their knowledge in a peer-to-peer
+            learning environment and build solutions for local businesses and
+            their community.
+          </Typography>
+        </CardContent>
+        <CardMedia
+          component="img"
+          heigh="140"
+          alt="team picture"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXRUnyNvt35eNGpley13X7C-lXCgT7GZC9uA&usqp=CAU"
+        />
+      </Stack>
+    </Card>
+  </Box>
+);
+
+export default About;
