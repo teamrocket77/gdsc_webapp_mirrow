942a3d3b528cced3340a98884d4cd12990ee3fda
Aimi <90480687+aimiisme@users.noreply.github.com>
2022-08-23 20:11:25 -0400

    Implement Responsive Navbar (mobile) and Auto-rescaling Web Content (#24)
    * Bump minimist from 1.2.5 to 1.2.6 in /src
    Did rebase b/c conflicts in dependencies
    
    Bumps [minimist](https://github.com/substack/minimist) from 1.2.5 to 1.2.6.
    - [Release notes](https://github.com/substack/minimist/releases)
    - [Commits](https://github.com/substack/minimist/compare/1.2.5...1.2.6)
    
    ---
    updated-dependencies:
    - dependency-name: minimist
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump next-auth from 4.2.1 to 4.9.0 in /src
    
    Bumps [next-auth](https://github.com/nextauthjs/next-auth) from 4.2.1 to 4.9.0.
    - [Release notes](https://github.com/nextauthjs/next-auth/releases)
    - [Changelog](https://github.com/nextauthjs/next-auth/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/nextauthjs/next-auth/compare/v4.2.1...next-auth@v4.9.0)
    
    ---
    updated-dependencies:
    - dependency-name: next-auth
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * adding responsive navbar feature and rescaling the main content
    
    * Replacing img tag with html tags and re-formating
    
    * adding about pages
    
    * Implement Button, Page mappings for Navbar.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Sanjay <Sanjay-B@users.noreply.github.com>
    Co-authored-by: Sanjay-B <sanjaybhadra1821@gmail.com>

---
+++ src/components/DrawerComp.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const DrawerComp = () => {
+  const PAGES = ["About", "Events", "Socials", "Our Team"];
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+    <>
+      <Drawer
+        PaperProps={{ sx: { width: 240 } }}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        <List>
+          {PAGES.map((page, index) => (
+            <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
+              <ListItemIcon>
+                <ListItemText>{page}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+        {" "}
+        <MenuIcon />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,138 @@
-import { Box, Grid, Typography, Stack, Button } from "@mui/material"
-import { useTheme } from "@mui/material/styles"
-import { useSession } from 'next-auth/react';
+import { Box, Grid, Typography, Stack, Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useSession } from "next-auth/react";
 
 export default function Index() {
-  const theme = useTheme()
+  const theme = useTheme();
   const { data: session, status } = useSession();
 
   // If the client session is loading, do this.
   // Todo: Replace with a nice loading screen.
   if (status === "loading") {
-    return <p>Loading Session</p>
+    return <p>Loading Session</p>;
   }
 
   return (
-    <Box component='div' sx={{ height: '100%', overflow: 'hidden' }} mt={4}>
-      <Grid container spacing={6} direction='column' alignItems='center' justifyContent='center' style={{ minHeight: '60vh' }}>
+    <Box
+      component="div"
+      sx={{ height: "100vh", width: "100vw", overflow: "hidden" }}
+      mt={4}
+    >
+      <Grid
+        container
+        spacing={6}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "60vh" }}
+      >
         <Grid item>
-          <Stack direction='row' spacing={1} display='flex' alignItems='center' flexWrap='wrap'>
-            <Box component='img' sx={{ height: 64, width: 64 }} alt='Google Developer Student Club Logo' src='/static/images/gdsc_logo.png' />
-            <Typography variant='h2' component='div' color={ theme.palette.primary.alternate } sx={{ flexGrow: 1}}>
+          <Stack
+            direction="row"
+            sx={{ overflow: "hidden" }}
+            justifyContent="center"
+            alignItems="center"
+            spacing={1.5}
+          >
+            <Box
+              component="img"
+              sx={{ height: "auto", width: "9%" }}
+              alt="Google Developer Student Club Logo"
+              src="/static/images/gdsc_logo.png"
+            />
+
+            <Typography
+              variant="h2"
+              component="div"
+              color={theme.palette.primary.alternate}
+              sx={{ fontSize: { xs: 22, sm: 36, md: 50, lg: 60 } }}
+            >
               Google Developer Student Club
             </Typography>
           </Stack>
-          <Typography variant='h3' component='div' color={ theme.palette.primary.alternate } sx={{ flexGrow: 1}} mt={1} ml={9.5}>
-            Kennesaw State University
-          </Typography>
-          <Stack direction='row' spacing={-20} display='flex' alignItems='center' flexWrap='wrap' mt={5} ml={21}>
-            <Typography variant='body1' component='div' color={ theme.palette.hashtags.main } sx={{ flexGrow: 1}}>
-              #developerstudentclubs
+          <Stack spacing={4} justifyContent="center" alignItems="center">
+            <Typography
+              variant="h3"
+              component="div"
+              color={theme.palette.primary.alternate}
+              sx={{ fontSize: { xs: 20, sm: 26, md: 26, lg: 36 } }}
+            >
+              Kennesaw State University
             </Typography>
-            <Typography variant='body1' component='div' color={ theme.palette.hashtags.main } sx={{ flexGrow: 1}}>
-              #googlestudents
-            </Typography>
-            <Typography variant='body1' component='div' color={ theme.palette.hashtags.main } sx={{ flexGrow: 1}}>
-              #growwithgoogle
-            </Typography>
+            {/*-----Hashtag--------*/}
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Typography
+                sx={{ fontSize: { xs: 12, sm: 17, md: 17, lg: 17 } }}
+                component="div"
+                color={theme.palette.hashtags.main}
+              >
+                #developerstudentclubs
+              </Typography>
+
+              <Typography
+                sx={{ fontSize: { xs: 12, sm: 17, md: 17, lg: 17 } }}
+                component="div"
+                color={theme.palette.hashtags.main}
+              >
+                #googlestudents
+              </Typography>
+
+              <Typography
+                sx={{ fontSize: { xs: 12, sm: 17, md: 17, lg: 17 } }}
+                component="div"
+                color={theme.palette.hashtags.main}
+              >
+                #growwithgoogle
+              </Typography>
+            </Stack>
           </Stack>
         </Grid>
+        {/*----Button-------*/}
         <Grid item>
-          <Button variant='contained' color='secondary'>Get Started</Button>
+          <Button variant="contained" color="secondary">
+            Get Started
+          </Button>
         </Grid>
       </Grid>
-      <Grid container spacing={0} direction='column' alignItems='center' justifyContent='center' sx={{ ml: -70, mt: -0.7 }}>
+      {/*-----Wavesvg-------*/}
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ ml: -70, mt: -0.7 }}
+      >
         <Grid item sx={{ mt: 10, zIndex: 3 }}>
-          <Box component='img' sx={{ height: 250.61, width: 2671.24 }} alt='' src='/static/images/wave_1.svg'/>
+          <Box
+            component="img"
+            sx={{ height: 250.61, width: 2671.24 }}
+            alt=""
+            src="/static/images/wave.svg"
+          />
         </Grid>
         <Grid item sx={{ mt: -37, zIndex: 2 }}>
-          <Box component='img' sx={{ height: 250.61, width: 2671.24 }} alt='' src='/static/images/wave_2.svg'/>
+          <Box
+            component="img"
+            sx={{ height: 250.61, width: 2671.24 }}
+            alt=""
+            src="/static/images/wave.svg"
+          />
         </Grid>
         <Grid item sx={{ mt: -37, zIndex: 1 }}>
-          <Box component='img' sx={{ height: 250.61, width: 2671.24 }} alt='' src='/static/images/wave_3.svg' />
+          <Box
+            component="img"
+            sx={{ height: 250.61, width: 2671.24 }}
+            alt=""
+            src="/static/images/wave.svg"
+          />
         </Grid>
       </Grid>
     </Box>
-  )
+  );
 }
