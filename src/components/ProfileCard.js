76853a218192c34b06e0ab3c343198929deda52b
Aimi <90480687+aimiisme@users.noreply.github.com>
2022-09-12 16:36:43 -0400

    Implement Team Page (#35)
    Implement Team Page (#35)

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,74 @@
+import * as React from "react";
+import {
+  Card,
+  Typography,
+  Avatar,
+  Container,
+  Grid,
+  Stack,
+} from "@mui/material";
+//Need to install icon package: `npm install @mui/icons-material`
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LinkIcon from "@mui/icons-material/Link";
+import { red } from "@mui/material/colors";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+export default function profilecard() {
+  return (
+    <Container justifyContent="center" alignItems="center" maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+        columns={{ xs: 2, sm: 8, md: 16 }}
+      >
+        {Array.from(Array(4)).map((_, index) => (
+          <Grid
+            item
+            justifyContent="center"
+            alignItems="center"
+            display="flex"
+            xs={2}
+            sm={4}
+            md={4}
+            key={index}
+          >
+            {/* ------------ Card Content --------------- */}
+            <Card sx={{ maxWidth: 215, px: 5, py: 3 }}>
+              <Grid
+                container
+                spacing={1.5}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item>
+                  <Avatar
+                    mt={5}
+                    sx={{ bgcolor: red[500], width: 100, height: 100 }}
+                  >
+                    A
+                  </Avatar>
+                </Grid>
+                <Grid item>
+                  <Typography> Name</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography> Roles</Typography>
+                </Grid>
+                <Grid item>
+                  <Stack direction="row" spacing={2}>
+                    <LinkIcon />
+                    <LinkedInIcon />
+                    <GitHubIcon />
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
