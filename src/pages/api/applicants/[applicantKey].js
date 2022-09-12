84607a0640af1b77972a2f1cace08a056ff86166
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-09-12 15:48:18 -0400

    404 on requesting nonexistent applicant

--- src/pages/api/applicants/[applicantKey].js
+++ src/pages/api/applicants/[applicantKey].js
@@ -9,5 +9,13 @@ export default async function handler(req, res) {
   var query = req.query
   const {applicantKey} = query
   const docref = await db.collection('applicants').doc(applicantKey.toLowerCase()).get();
-  res.status(200).json(docref.data())
+  console.log("hi")
+  console.log(docref.data())
+  if(docref.data() == undefined)
+  {
+    res.status(404).send({message: 'Applicant with this key could not be found.'})
+  }
+  else{
+    res.status(200).json(docref.data())
+  }
 }
