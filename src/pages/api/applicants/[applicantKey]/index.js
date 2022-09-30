4157fd3ccf416909306c7d69b2b269ddf505130c
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-09-30 01:51:13 -0400

    Implement API endpoints for recruitment (#25)
    Read commit history for details.

---
+++ src/pages/api/applicants/[applicantKey]/index.js
@@ -0,0 +1,21 @@
+import { firestoreApp } from '../../../../database';
+import { getFirestore } from 'firebase-admin/firestore';
+const Firestore_Database = getFirestore(firestoreApp);
+
+export default async function handler(req, res) {
+  if(req.method !== 'GET')
+  {
+    res.status(405).send({message: 'Only GET requests at this endpoint, please'})
+    return;
+  }
+  var query = req.query
+  const {applicantKey} = query
+  const docref = await Firestore_Database.collection('applicants').doc(applicantKey.toLowerCase()).get();
+  if(docref.data() == undefined)
+  {
+    res.status(404).send({message: 'Applicant with this key could not be found.'})
+  }
+  else{
+    res.status(200).json(docref.data())
+  }
+}
