f43711ef683becee558dd5908241d9c33ea53ae8
Sanjay-B <sanjaybhadra1821@gmail.com>
2022-09-30 03:58:59 -0400

    Implement GET /api/events/all, display events to /events page.

--- src/pages/api/events/all.js
+++ src/pages/api/events/all.js
@@ -2,16 +2,31 @@ import { firestoreApp  } from '../../../database';
 import { getFirestore } from 'firebase-admin/firestore';
 const Firestore_Database = getFirestore(firestoreApp);
 
+export async function getAllEvents() {
+    const snapshot = await Firestore_Database.collection('events').get();
+    return snapshot.docs.map(doc => doc.data());
+}
+
 export default async function handler(req, res) {
     if (req.method === 'GET') {
-        const snapshot = await Firestore_Database.collection('events').get();
-        res.status(200).json(JSON.stringify(snapshot.docs.map(doc => doc.data())));
+        res.status(200).json(await getAllEvents().then(function(data) { return data }));
     }
 }
 
 // Event (id: int)
 // -> title: (string)
-// -> location: (hyperlink: string)
-// -> time: (string)
+// -> location: (map)
+//  -> type: (string)
+//  -> data: (string)
+// -> date: (map)
+//  -> weekday: (string)
+//  -> day: (string)
+//  -> month: (string)
+//  -> year: (string)
+//  -> time: (map)
+//   -> start: (string)
+//   -> end: (string)
+//   -> timezone: (string)
 // -> description: (string)
-// -> calendarLink: (hyperlink: string) 
+// -> calendar: (map) 
+//  -> link: (hyperlink: string))
