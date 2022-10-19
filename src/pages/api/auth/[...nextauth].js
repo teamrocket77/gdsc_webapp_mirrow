a720b98edb1051ad6db2175687c26b7b1880465a
Sanjay-B <sanjaybhadra1821@gmail.com>
2022-10-19 02:33:19 -0400

    Removed dotenv because nextjs does the same automatically.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,14 +2,6 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { FirestoreAdapter } from '@next-auth/firebase-adapter';
 
-// Configure Environmental Variables.
-// NODE_ENV is set to 'development' when 'npm run dev' in development mode.
-// NODE_ENV is set to 'production' when 'npm start' in production mode.
-// DEVELOPERS WILL ALWAYS BE IN DEVELOPMENT MODE.
-const dotenv = require('dotenv');
-dotenv.config({path: `.env.${process.env.NODE_ENV}`});
-console.log('Environment Mode: '+process.env.NODE_ENV);
-
 // Configure Authentication Provider.
 export default NextAuth({
     providers: [
