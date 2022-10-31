b93119a3746048387222d1d65c2a89d24dd59c7d
Sanjay-B <sanjaybhadra1821@gmail.com>
2022-10-31 04:09:11 -0400

    Disable FirestoreAdapter for Authentication.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { FirestoreAdapter } from '@next-auth/firebase-adapter';
+// import { FirestoreAdapter } from '@next-auth/firebase-adapter';
 
 // Configure Authentication Provider.
 export default NextAuth({
