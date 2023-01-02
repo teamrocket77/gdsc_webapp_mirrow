77b51c70037364da96e30fcc28f063bf92cc69c1
teamrocket77 <vincentcradler@gmail.com>
2023-01-02 18:34:23 -0500

    New Github Action to run Cypress Front end Tests (#62)
    Ref: https://github.com/KSU-Google-DSC/Webapp/pull/62

---
+++ src/cypress/e2e/frontend_tests/frontend.screenshots.cy.js
@@ -0,0 +1,30 @@
+describe('Get Screenshots', () => {
+	let screenshot_options = {
+		capture : "fullPage",
+	};
+	let get_options = {
+		"timeout" : 10000, 
+	};
+	it('passes', () => {
+		let webPath = 'http://localhost:3000';
+		cy.log("Beginning Screenshot test");
+		cy.visit(webPath);
+    		cy.get('[data-cy = "nav-bar-option-About"', get_options)
+			.should('be.visible');
+		cy.screenshot('base', screenshot_options);
+		cy.fixture('../fixtures/pages.json').then((page_json) => {
+			let pages = page_json["pages"];
+			for (var page in pages){
+				let baseSubPage = pages[page];
+				let subPage = webPath + "/" + baseSubPage;
+				cy.log(`Visiting subpage: ${subPage}`);
+				cy.visit(subPage);
+				cy.get('[data-cy = "nav-bar-option-About"', get_options)
+					.should('be.visible');
+				cy.screenshot(baseSubPage , screenshot_options);
+
+			}
+		});
+	})
+})
+
