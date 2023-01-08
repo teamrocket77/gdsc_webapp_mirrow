c5c6b8e3c738dd784b1640319f903e92ffbf2f4c
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2023-01-08 15:36:22 -0500

    Convert all endpoints into a handler that calls an exported async function (#68)
    Ref: https://github.com/KSU-Google-DSC/Webapp/pull/68

--- src/pages/api/applicants/index.js
+++ src/pages/api/applicants/index.js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
       res.status(405).send({message: 'Only GET requests at this endpoint, please'})
       return;
     }
+    res.status(200).json(await getAllApplicants());
+}
+export async function getAllApplicants(){
     const snapshot = await Firestore_Database.collection('applicants').get();
-    res.status(200).json(JSON.stringify(snapshot.docs.map(doc => doc.data())));
+    return snapshot.docs.map(doc => doc.data());
 }
