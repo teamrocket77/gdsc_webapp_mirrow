1e0801c3825045a6386361f0bb06ffe7c8eab035
teamrocket77 <vincentcradler@gmail.com>
2023-01-17 20:06:23 -0500

    changed frontend nav test to be more dynamic, should not work with added and removing pages as opposed to being hardcoded

--- src/cypress/e2e/frontend_tests/frontend.nav_test.cy.js
+++ src/cypress/e2e/frontend_tests/frontend.nav_test.cy.js
@@ -1,24 +1,39 @@
 describe('Nav Bar Test', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000')
+    const home = 'http://localhost:3000';
+    cy.visit(home);
+    //ensure that nav bar is visible
+    cy.get('a[data-cy^=nav-bar-option]').should('be.visible')
     cy.log('Visiting base page')
 
-    cy.get('[data-cy = "nav-bar-option-About"').click()
+    //create links and cy-data list from home page as shown 
+    let links = []
+    let cy_data = []
+    //get all sub pages that have links in navbar
+    cy.get('[data-cy^=nav-bar-option-]').each(
+      ($val) =>{
+        // get href attr and data-cy
+        cy.wrap($val).invoke('attr', 'href').then(
+          (href) => {
+            links.push(href)
+          }
+        )
+        cy.wrap($val).invoke('attr', 'data-cy').then(
+          (data) => {
+            cy_data.push(data)
+          }
+        )
+      }
+    );
 
-    cy.location('pathname').should('eq', '/about');
-    cy.log('Visiting about page')
-
-    cy.get('[data-cy = "nav-bar-option-Socials"').click()
-
-    cy.location('pathname').should('eq', '/socials');
-    cy.log('Visiting base page')
-
-
-    cy.get('[data-cy = "nav-bar-option-Our Team"').click()
-
-    cy.location('pathname').should('eq', '/team');
-    cy.log('Visiting team page')
-
-  })
+    //wrap list so it can be used by cypress
+    cy.wrap(links).each((item) => {
+      cy.visit(home + item);
+      cy.get('a[data-cy^=nav-bar-option]').should('be.visible');
+      cy.wrap(cy_data).each((data_item) => {
+        cy.get(`[data-cy=${data_item}]`).should('be.visible');
+      });
+    });
+  });
 })
 
