f2ec822678681e8ed3b009f1281cee8ce5d5914c
teamrocket77 <vincentcradler@gmail.com>
2023-01-25 15:57:33 -0500

    configured nextjs wit additional security options

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,33 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+];
+
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     esmExternals: false
+  },
+  async headers() {
+    return [
+      {
+        //source: "/:path*",
+        source: "/",
+        headers: securityHeaders,
+      }
+    ]
   }
 }
 
