e955b557338e34f608151a31b60faf8c4800f87b
teamrocket77 <vincentcradler@gmail.com>
2023-01-31 00:12:22 -0500

    removed linting issues

--- src/cypress/e2e/frontend_tests/frontend.dashboard_login.cy.js
+++ src/cypress/e2e/frontend_tests/frontend.dashboard_login.cy.js
@@ -1,12 +1,12 @@
 describe('This test is attempting to log into the dashboard', () => {
- 	it('passes', () => {
- 		let webPath = 'http://localhost:3000/login';
- 		cy.log("Beginning Login test");
- 		cy.visit(webPath);
- 		// type pertinent information in for testing
-     		cy.get('[id=username]').type('admin');
-     		cy.get('[id=password').type('admin');
- 		// submit page redirected to 
-     		cy.get('[type=submit').click();
- 	})
- })
+	it('passes', () => {
+		let webPath = 'http://localhost:3000/login';
+		cy.log("Beginning Login test");
+		cy.visit(webPath);
+		// type pertinent information in for testing
+			cy.get('[id=username]').type('admin');
+			cy.get('[id=password').type('admin');
+		// submit page redirected to 
+			cy.get('[type=submit').click();
+	})
+})
