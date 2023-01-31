e7e9e52ab166f9dd19ee5407e52bec3dbb5fca40
IsaacAlao <isaacmobolajialao@gmail.com>
2023-01-31 13:52:25 -0500

    Update cypress.config.js

--- src/cypress.config.js
+++ src/cypress.config.js
@@ -1,22 +1,21 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // for generating videos
-  video : false,
-
+  video: false,
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
-    baseUrl : 'http://localhost:3000',
-	  env :{
-		  codeCoverage:  {
-			  url :  "/api/__coverage__"
-		  }
-	  },
+    baseUrl: "http://localhost:3000",
+    env: {
+      codeCoverage: {
+        url: "/api/__coverage__",
+      },
+    },
     setupNodeEvents(on, config) {
-	    require('@cypress/code-coverage/task')(on, config)
+      require("@cypress/code-coverage/task")(on, config);
       // implement node event listeners here
-      return config
+      return config;
     },
   },
-})
+});
